75a88c1cf1a1955b0e631f90c8ac78a10262593e
Adduri-prameela <prameelaadduri2005@gmail.com>
2026-06-05 09:38:27 +0530

    Add files via upload

---
+++ demo84.py
@@ -0,0 +1,7 @@
+lang={'JAva','Python','C','C++','SQL','c'}
+print(lang)
+lang.add('Ruby')
+print(lang)
+ui={'HTML','Express js','Amgular'}
+lang.update(ui)
+print(lang)
---
+++ demo87.py
@@ -0,0 +1,10 @@
+Lang={
+    'UI/UX':['HTML','CSS','JavaScript','React js','Express js'],
+    'Backend':['Python','Flask','Django','Pyramid','Bottle'],
+    'Server':['SQL','Pl-Sql','Ms-SQL']
+}
+print(Lang['UI/UX'])
+print(Lang['Backend'])
+print(Lang['Server'])
+Lang['Server']=['Oracle-SQL']
+print(Lang['Server'])
